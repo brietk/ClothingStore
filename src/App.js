5e46d95c58bcc9df4f62a7f3e318eaad62824987
brietk <brietkonn@gmail.com>
2021-07-16 14:26:11 +0000

    implemented firebase utils, included ability to store authenticated users into firestore database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -21,11 +21,24 @@ class App extends React.Component {
 
   //keeps the user signed in until he signs out
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
-    })
+        userRef.onSnapshot(onSnapshot => {
+          this.setState({
+            currentUser: {
+              id: onSnapshot.id,
+              ...onSnapshot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
 
   //this will close the open subscribtion on firebase
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,47 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-        apiKey: "AIzaSyAKF50wkZa9z7kGJoRdc3CJND4bLCVzc2A",
-        authDomain: "crown-db-bdff1.firebaseapp.com",
-        projectId: "crown-db-bdff1",
-        storageBucket: "crown-db-bdff1.appspot.com",
-        messagingSenderId: "70346755105",
-        appId: "1:70346755105:web:ba36835cac67a9177ae760",
-        measurementId: "G-9M9ZQ78TDY"
+    apiKey: "AIzaSyAKF50wkZa9z7kGJoRdc3CJND4bLCVzc2A",
+    authDomain: "crown-db-bdff1.firebaseapp.com",
+    projectId: "crown-db-bdff1",
+    storageBucket: "crown-db-bdff1.appspot.com",
+    messagingSenderId: "70346755105",
+    appId: "1:70346755105:web:ba36835cac67a9177ae760",
+    measurementId: "G-9M9ZQ78TDY"
 }
+
+//put what we get from firebase - the userAuth into the async.
+//if there is no userAuth, return. Else - save to database.
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    //if the user does not exist, than put him in the db
+    if(!snapShot.exists) {
+        //first, what data to store?
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        //now we do an async request to db to store the data. So
+        //we use try catch block
+
+        try {
+            await userRef.set({
+                    displayName,
+                    email,
+                    createdAt,
+                    ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+
+    }
+    return userRef;
+};
 
 firebase.initializeApp(config);
 
