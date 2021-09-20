94fb2c908fb1490a8802edb8dfa7abf7683dfbab
brietk <brietkonn@gmail.com>
2021-09-20 21:43:21 +0000

    adding cache storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,23 @@
 import { combineReducers } from "redux";
 
-import userReducer from "./user/user.reducer";
 
+import { persistReducer } from "redux-persist";
+//the actual local storage
+import storage from "redux-persist/lib/storage";
+
+import userReducer from "./user/user.reducer";
 import cartReduser from "./cart/cart.reducer";
 
-//a function from redux that creates the root reducer object
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReduser
 });
+
+//a function from redux that creates the root reducer object
+export default persistReducer(persistConfig, rootReducer);
